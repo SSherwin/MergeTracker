38d4380c15694ef06da4f39d0e53d70c7ed06c90
Steve <Steve@buhtig.com>
2014-11-30 16:22:51 +0000

    Create a provider for test data to use this services.

---
+++ src/main/java/buhtig/steve/mergetracker/providers/IMergeTrackerDataProvider.java
@@ -0,0 +1,24 @@
+package buhtig.steve.mergetracker.providers;
+
+import buhtig.steve.mergetracker.model.BranchMergeTracker;
+
+import java.util.TreeMap;
+
+/**
+ * Created by Steve on 30/11/2014.
+ */
+public interface IMergeTrackerDataProvider {
+
+    /**
+     *
+     * @return map of merge reports
+     */
+    TreeMap<String, BranchMergeTracker> loadData();
+
+    /**
+     *
+     * @param branchMergeTracker tracker to be refreshed
+     */
+    void refresh(final BranchMergeTracker branchMergeTracker);
+}
+
---
+++ src/main/java/buhtig/steve/mergetracker/providers/TestDataProvider.java
@@ -0,0 +1,100 @@
+package buhtig.steve.mergetracker.providers;
+
+import buhtig.steve.mergetracker.model.Branch;
+import buhtig.steve.mergetracker.model.BranchMergeTracker;
+import buhtig.steve.mergetracker.model.Revision;
+import buhtig.steve.mergetracker.providers.IMergeTrackerDataProvider;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.TreeMap;
+
+/**
+ * Created by Steve on 30/11/2014.
+ */
+@Component
+public class TestDataProvider implements IMergeTrackerDataProvider {
+
+
+    @Override
+    public TreeMap<String, BranchMergeTracker> loadData() {
+        final Branch release1 = new Branch("branches/RELEASE-1.0.0");
+        final Branch release2 = new Branch("branches/RELEASE-2.0.0");
+        final Branch trunk = new Branch("trunk");
+        final Branch feature = new Branch("branches/FEATURE1");
+
+        BranchMergeTracker merge1 = new BranchMergeTracker(release2, release1);
+        BranchMergeTracker merge2 = new BranchMergeTracker(feature, trunk);
+        BranchMergeTracker merge3 = new BranchMergeTracker(trunk, feature);
+        BranchMergeTracker merge4 = new BranchMergeTracker(trunk, release2);
+
+
+        //Add 20 revisions to trunk
+        for(long rev =1; rev <= 20; rev++) {
+            Revision revision = new Revision(rev, "Steve", "Messages 1", new Date());
+            trunk.addRevision(revision);
+        }
+
+        for(long rev =21; rev <= 25; rev++) {
+            Revision revision = new Revision(rev, "Steve", "Messages 2", new Date());
+            release1.addRevision(revision);
+        }
+
+        for(long rev =26; rev <= 30; rev++) {
+            Revision revision = new Revision(rev, "Steve", "Messages 3", new Date());
+            trunk.addRevision(revision);
+        }
+
+        for(long rev =31; rev <= 35; rev++) {
+            Revision revision = new Revision(rev, "Steve", "Messages 4", new Date());
+            release1.addRevision(revision);
+        }
+
+        for(long rev =36; rev <= 40; rev++) {
+            Revision revision = new Revision(rev, "Bob", "Messages 5", new Date());
+            trunk.addRevision(revision);
+        }
+
+        for(long rev =41; rev <= 45; rev++) {
+            Revision revision = new Revision(rev, "Bob", "Messages 6", new Date());
+            release2.addRevision(revision);
+        }
+
+        for(long rev =46; rev <= 60; rev++) {
+            Revision revision = new Revision(rev, "Bob", "Messages 6", new Date());
+            trunk.addRevision(revision);
+        }
+
+        for(long rev =601; rev <= 80; rev++) {
+            Revision revision = new Revision(rev, "Bob", "Messages 6", new Date());
+            if (rev%2 == 0) {
+                trunk.addRevision(revision);
+                merge2.addRevision(rev);
+            } else {
+                feature.addRevision(revision);
+                merge3.addRevision(rev);
+            }
+        }
+
+
+        merge1.addRevision(22L);
+        merge1.addRevision(23L);
+
+        TreeMap<String, BranchMergeTracker> result = new TreeMap<>();
+        result.put(merge1.getTitle(), merge1);
+        result.put(merge2.getTitle(), merge2);
+        result.put(merge3.getTitle(), merge3);
+        result.put(merge4.getTitle(), merge4);
+
+
+        return result;
+    }
+
+    @Override
+    public void refresh(BranchMergeTracker branchMergeTracker) {
+
+    }
+}
+
