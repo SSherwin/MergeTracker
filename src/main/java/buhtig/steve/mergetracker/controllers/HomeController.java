ccb1e4c4186f38e6e7464b524477ffc18e0ef618
steve <steve@me.com>
2015-01-13 20:50:25 +0000

    More testing of the use of thymeleaf - appears to be a workable solution...

--- src/main/java/buhtig/steve/mergetracker/controllers/HomeController.java
+++ src/main/java/buhtig/steve/mergetracker/controllers/HomeController.java
@@ -1,11 +1,24 @@
 package buhtig.steve.mergetracker.controllers;
 
+import buhtig.steve.mergetracker.model.BranchMergeTracker;
+import buhtig.steve.mergetracker.services.MergerTrackerReportServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 @Controller
+@Configurable
 class HomeController {
+
+    @Autowired
+    MergerTrackerReportServices mergeService;
 
     @SuppressWarnings("SameReturnValue")
     @RequestMapping("/")
@@ -17,5 +30,13 @@ class HomeController {
     @ResponseBody
     java.util.Properties properties() {
         return System.getProperties();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value ="/listmerge")
+    public String list(Model model) {
+        model.addAttribute("mergelist", mergeService.list());
+
+
+        return "listmerge";
     }
 }
